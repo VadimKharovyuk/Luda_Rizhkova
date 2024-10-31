35de89c7fb9bdd52acf22368e172c2cbf3b63811
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-11-01 02:08:56 +0400

    fix

--- src/main/java/com/example/luda_rizhkova/LudaController.java
+++ src/main/java/com/example/luda_rizhkova/LudaController.java
@@ -1,4 +1,10 @@
 package com.example.luda_rizhkova;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class LudaController {
+    public String homePage(){
+        return "index.html";
+    }
 }
--- src/main/java/com/example/luda_rizhkova/LudaRizhkovaApplication.java
+++ src/main/java/com/example/luda_rizhkova/LudaRizhkovaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LudaRizhkovaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LudaRizhkovaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LudaRizhkovaApplication.class, args);
+    }
 
 }
